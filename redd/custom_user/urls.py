873d2cafa5dfc688738df02553319976757d92d3
jredd <jredd46@gmail.com>
2016-03-30 14:34:01 -0500

    fixed user view bug where the root url wasn't accessing the view properly

--- redd/custom_user/urls.py
+++ redd/custom_user/urls.py
@@ -5,6 +5,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'$', views.UsersList.as_view(), name='user_list'),
+    url(r'^$', views.UsersList.as_view(), name='user_list'),
     url(r'(?P<pk>.+)/$', views.UserDetail.as_view(), name='user_detail'),
 ]
